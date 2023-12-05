1f18f59a31beabeaaa313760ec74dcfb4f3c8f13
Kiwifruittttt <kaiwee927@gmail.com>
2023-12-05 11:13:54 +0800

    matrices suck

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
@@ -1,12 +1,19 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Util
-// {
-//     public static float FindDistance(HVector2D p1, HVector2D p2)
-//     {
-//         return /*your code here*/;
-//     }
-// }
+public class Util
+{
+    public static float FindDistance(HVector2D p1, HVector2D p2)
+    {
+        return Mathf.Sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+        HVector2D a = new HVector2D(8f, 5f);
+        HVector2D b = new HVector2D(1f, 3f);
+        float distance = Util.FindDistance(a, b);
+
+
+
+    }
+}
 
